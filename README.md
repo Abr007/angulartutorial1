cf62f12c999149e39aa67212622868bb246008c5
Abr007 <34203489+Abr007@users.noreply.github.com>
2023-06-20 19:51:49 -0400

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# angulartutorial1
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/3m4hs3)
---
+++ src/app/home/home.component.ts
@@ -0,0 +1,23 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { HousingLocationComponent } from '../housing-location/housing-location.component';
+import { Housinglocation } from '../housinglocation';
+@Component({
+  selector: 'app-home',
+  standalone: true,
+  imports: [CommonModule, HousingLocationComponent],
+  templateUrl: './home.component.html',
+  styleUrls: ['./home.component.css'],
+})
+export class HomeComponent {
+  housingLocation: Housinglocation = {
+    id: 9999,
+    name: 'Test Home',
+    city: 'Test city',
+    state: 'ST',
+    photo: 'assets/home1.svg',
+    availableUnits: 99,
+    wifi: true,
+    laundry: false,
+  };
+}
